22e063b3d8aeabceede77a23d0517eeda432217b
Brr <brrshh@gmail.com>
2023-03-28 16:40:09 +0300

    Initial commit

---
+++ src/main/java/by/guavapay/GuavapayApplication.java
@@ -0,0 +1,13 @@
+package by.guavapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavapayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavapayApplication.class, args);
+    }
+
+}
